c9c20a43733359a9b559678c1e061a5b1d03dd04
Jaroslaw Pawlak <jaroslaw.pawlak@zuhlke.com>
2015-02-05 10:59:34 +0000

    implemented checkout for multiple items of different type

--- src/main/java/checkoutkata2/CheckoutSystem.java
+++ src/main/java/checkoutkata2/CheckoutSystem.java
@@ -1,9 +1,20 @@
 package checkoutkata2;
 
+import static java.util.Arrays.stream;
+
 public class CheckoutSystem {
 
+    @SuppressWarnings("Convert2MethodRef")
     public int checkout(String... items) {
-        return 60 * items.length;
+        return stream(items).mapToInt(item -> priceOf(item)).sum();
+    }
+
+    private static int priceOf(String item) {
+        if (item.equals("Apple")) {
+            return 60;
+        } else {
+            return 25;
+        }
     }
 
 }
--- src/test/java/checkoutkata2/CheckoutSystemTest.java
+++ src/test/java/checkoutkata2/CheckoutSystemTest.java
@@ -8,20 +8,30 @@ import static org.hamcrest.CoreMatchers.is;
 
 public class CheckoutSystemTest {
 
+    private static final int APPLE_PRICE = 60;
+    private static final int ORANGE_PRICE = 25;
+
     private final CheckoutSystem checkoutSystem = new CheckoutSystem();
 
     @Test
     public void calculatesTotalCostOfSingleItem() {
         int totalPrice = checkoutSystem.checkout("Apple");
 
-        assertThat(totalPrice, is(equalTo(60)));
+        assertThat(totalPrice, is(equalTo(APPLE_PRICE)));
     }
 
     @Test
     public void calculatesTotalCostOfTwoSameItems() {
         int totalPrice = checkoutSystem.checkout("Apple", "Apple");
 
-        assertThat(totalPrice, is(equalTo(120)));
+        assertThat(totalPrice, is(equalTo(APPLE_PRICE * 2)));
+    }
+
+    @Test
+    public void calculatesTotalCostOfMultipleVariousItems() {
+        int totalPrice = checkoutSystem.checkout("Apple", "Apple", "Orange", "Apple");
+
+        assertThat(totalPrice, is(equalTo(APPLE_PRICE * 3 + ORANGE_PRICE)));
     }
 
 }
